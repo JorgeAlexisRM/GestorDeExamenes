7412031b615415c60cb34e71d11810fc0d6a669a
JorgeAlexisRM <alex.jruam@gmail.com>
2023-10-18 22:29:43 -0600

    "Editando configuracion"

--- js/configuracion.js
+++ js/configuracion.js
@@ -1,12 +1,6 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 firebase.initializeApp({
-    apiKey: "AIzaSyAOjtsUP_WObegMi1qFiuMPeWjGfl5rbps",
-    authDomain: "gestor-de-examenes.firebaseapp.com",
-    projectId: "gestor-de-examenes",
-    storageBucket: "gestor-de-examenes.appspot.com",
-    messagingSenderId: "519988131172",
-    appId: "1:519988131172:web:870a3d62e32fcdc5043671",
-    measurementId: "G-P1H1FGBWB7"
+    //credenciales
 });
 
 // Initialize Cloud Firestore and get a reference to the service
