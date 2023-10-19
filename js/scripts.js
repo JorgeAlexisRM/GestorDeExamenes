62539914af697641c5cc6db98d88aaa31897be02
gabriel123as <juniorpitufo1994@gmail.com>
2023-10-19 13:55:34 -0600

    botones

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,24 @@
+const FormLogin = document.getElementById('Login');
+const FormRegistro = document.getElementById('Registro');
+const botonLogin = document.getElementById('btnlogin');
+const botonRegistro = document.getElementById('btnRegistro');
+
+botonLogin.style.borderBlockEnd = '2px solid';
+FormRegistro.style.display = 'none';
+botonLogin.addEventListener('click', () => {
+    FormLogin.style.display = 'block';
+    FormRegistro.style.display = 'none';
+    botonLogin.style.borderBlockEnd = '2px solid';
+    botonRegistro.style.borderBlockEnd = '0px solid';
+});
+
+botonRegistro.addEventListener('click', () => {
+    FormLogin.style.display = 'none';
+    FormRegistro.style.display = 'block';
+    botonRegistro.style.borderBlockEnd = '2px solid';
+    botonLogin.style.borderBlockEnd = '0px solid';
+});
+
     /* boton modo claro y oscuro*/
     const darkModeToggle = document.getElementById('dark-mode-toggle');
     const formulario = document.getElementById('Formu');
